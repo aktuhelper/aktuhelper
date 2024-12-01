b9cf68b4361ee86c630f4e0205f0d669cced5f9f
Aditya Singh <119118069+aktuhelper@users.noreply.github.com>
2024-12-01 21:29:19 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,26 @@
+
+if('serviceWorker' in navigator){
+navigator.serviceWorker.register('/se.js')
+.then(function(){
+  console.log('SW Registered');
+});
+}
+addEventListener('click', function (event) {
+  if (!detailsShown) {
+    detailsShown = true;
+    pwaCardContent.style.opacity = 0;
+    pwaCardDetails.style.display = 'block';
+    pwaCardContent.style.display = 'none';
+    setTimeout(function () {
+      pwaCardDetails.style.opacity = 1;
+    }, 300);
+  } else {
+    detailsShown = false;
+    pwaCardDetails.style.opacity = 0;
+    pwaCardContent.style.display = 'block';
+    pwaCardDetails.style.display = 'none';
+    setTimeout(function () {
+      pwaCardContent.style.opacity = 1;
+    }, 300);
+  }
+});
